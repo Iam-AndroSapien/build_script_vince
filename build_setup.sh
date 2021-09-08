16fc0492e19b43c362b4660e54266baf36dd65ab
andro-sapien <alanmadathil007@gmail.com>
2021-09-08 09:51:08 +0530

    script: change wlan
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -16,6 +16,7 @@ echo " Now some miscellaneous things"
 git clone https://github.com/andro-sapien/vendor_prebuilts_dirac.git vendor/xiaomi/dirac 
 rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf && git clone https://github.com/andro-sapien/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
 rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi && git clone https://github.com/andro-sapien/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
+rm -rf hardware/qcom-caf/wlan && git clone https://android.googlesource.com/platform/hardware/qcom/wlan hardware/qcom-caf/wlan
 echo "..."
 echo "..."
 echo "finishing...."
