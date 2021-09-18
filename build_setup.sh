6e58c792cf8e354928a49e810d80cbd4ee8ed541
andro-sapien <alanmadathil007@gmail.com>
2021-09-18 08:56:01 +0530

    Revert "script: Nuke HALs"
    This reverts commit 13ff01d08c2e22dac005a499ee0bfb199e3b259e.

--- build_setup.sh
+++ build_setup.sh
@@ -5,6 +5,13 @@ echo "Kernel tree.."
 git clone --depth 1 https://github.com/Blacksuan19/kernel_dark_ages_vince.git -b darky kernel/xiaomi/vince
 echo "..."
 echo "..."
+echo "Setting up hals...."
+rm -rf hardware/qcom-caf/msm8996
+git clone https://github.com/andro-sapien/hardware_qcom_audio_msm8996.git hardware/qcom-caf/msm8996/audio
+git clone https://github.com/andro-sapien/hardware_qcom_display_msm8996.git hardware/qcom-caf/msm8996/display
+git clone https://github.com/andro-sapien/hardware_qcom_media_msm8996.git hardware/qcom-caf/msm8996/media
+echo "..."
+echo "..."
 echo " Now some miscellaneous things"
 git clone https://github.com/andro-sapien/vendor_prebuilts_dirac.git vendor/xiaomi/dirac 
 rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf && git clone --depth 1 https://github.com/andro-sapien/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
