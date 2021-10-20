7af2284b72dd1e8f6299b9fddece6591df33b9c7
andro-sapien <alanmadathil007@gmail.com>
2021-10-20 21:05:31 +0530

    scripts: Update
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -2,7 +2,7 @@
 cd ../
 echo "starting setting up device side repo...."
 echo "Kernel tree.."
-git clone --depth 1 https://github.com/Blacksuan19/kernel_dark_ages_vince.git -b darky kernel/xiaomi/vince
+git clone --depth 1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git -b stable kernel/xiaomi/vince
 echo "..."
 echo "..."
 echo "Setting up hals...."
@@ -14,8 +14,7 @@ echo "..."
 echo "..."
 echo " Now some miscellaneous things"
 git clone https://github.com/andro-sapien/vendor_prebuilts_dirac.git vendor/xiaomi/dirac 
-rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf && git clone --depth 1 https://github.com/andro-sapien/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
-rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi && git clone --depth 1 https://github.com/andro-sapien/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
+git clone --depth 1 https://github.com/andro-sapien/vortex-clang.git prebuilts/clang/host/linux-x86/clang-14
 rm -rf hardware/qcom-caf/wlan && git clone https://android.googlesource.com/platform/hardware/qcom/wlan hardware/qcom-caf/wlan
 echo "..."
 echo "..."
