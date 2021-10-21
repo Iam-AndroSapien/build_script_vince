fb452f54cd9c40c411447c44f872fcc07c320091
andro-sapien <alanmadathil007@gmail.com>
2021-10-21 20:13:48 +0530

    script: Drop hals
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -5,11 +5,6 @@ echo "Kernel tree.."
 git clone --depth 1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git -b stable kernel/xiaomi/vince
 echo "..."
 echo "..."
-echo "Setting up hals...."
-rm -rf hardware/qcom-caf/msm8996
-git clone https://github.com/andro-sapien/hardware_qcom_audio_msm8996.git hardware/qcom-caf/msm8996/audio
-git clone https://github.com/andro-sapien/hardware_qcom_display_msm8996.git hardware/qcom-caf/msm8996/display
-git clone https://github.com/andro-sapien/hardware_qcom_media_msm8996.git hardware/qcom-caf/msm8996/media
 echo "..."
 echo "..."
 echo " Now some miscellaneous things"
