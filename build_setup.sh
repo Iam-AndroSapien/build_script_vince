ea97207c124007cc21675c58d47e3397b5108e24
andro-sapien <alanmadathil007@gmail.com>
2022-01-12 12:56:53 +0530

    scripts: Update repos
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -2,14 +2,17 @@
 cd ../
 echo "starting setting up device side repo...."
 echo "Kernel tree.."
-git clone --depth 1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git -b stable kernel/xiaomi/vince
+git clone --depth 1 https://github.com/Blacksuan19/kernel_dark_ages_vince.git -b darky kernel/xiaomi/vince
 echo "..."
 echo "..."
 echo "..."
 echo "..."
 echo " Now some miscellaneous things"
-git clone https://github.com/andro-sapien/vendor_prebuilts_dirac.git vendor/xiaomi/dirac 
-rm -rf hardware/qcom-caf/wlan && git clone https://android.googlesource.com/platform/hardware/qcom/wlan hardware/qcom-caf/wlan
+rm -rf hardware/qcom-caf/msm8996
+git clone https://github.com/andro-sapien/hardware_qcom_audio_msm8996.git -b 12 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/andro-sapien/hardware_qcom_display_msm8996.git -b 12 hardware/qcom-caf/msm8996/display
+git clone https://github.com/andro-sapien/hardware_qcom_media_msm8996.git -b r11.0 hardware/qcom-caf/msm8996/media 
+rm -rf hardware/qcom-caf/wlan && git clone https://github.com/andro-sapien/hardware_qcom_wlan.git -b 12 hardware/qcom-caf/wlan
 echo "..."
 echo "..."
 echo "finishing...."
