6a9f0d8486ca9c53430ec27fddbac324f8b72904
andro-sapien <alanmadathil007@gmail.com>
2022-02-01 20:04:10 +0530

    now we are building with unitrix

--- build_setup.sh
+++ build_setup.sh
@@ -2,7 +2,7 @@
 cd ../
 echo "starting setting up device side repo...."
 echo "Kernel tree.."
-git clone --depth 1 https://github.com/Blacksuan19/kernel_dark_ages_vince.git -b darky kernel/xiaomi/vince
+git clone --depth 1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git -b stable kernel/xiaomi/vince
 echo "..."
 echo "..."
 echo "..."
@@ -17,8 +17,5 @@ echo "..."
 echo "..."
 echo "..."
 echo "..."
-echo " Eva GCC"
-rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf && git clone --depth 1 https://github.com/andro-sapien/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
-rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi && git clone --depth 1 https://github.com/andro-sapien/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
 echo "finishing...."
 echo "All Done."
