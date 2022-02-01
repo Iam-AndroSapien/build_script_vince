ad74bbd8452bd2ff042cee95dc2add91df11176d
andro-sapien <alanmadathil007@gmail.com>
2022-02-01 09:54:48 +0530

    script: Update branches
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -9,9 +9,9 @@ echo "..."
 echo "..."
 echo " Now some miscellaneous things"
 rm -rf hardware/qcom-caf/msm8996
-git clone https://github.com/andro-sapien/hardware_qcom_audio_msm8996.git -b 12 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/andro-sapien/hardware_qcom_display_msm8996.git -b 12 hardware/qcom-caf/msm8996/display
-git clone https://github.com/andro-sapien/hardware_qcom_media_msm8996.git -b r11.0 hardware/qcom-caf/msm8996/media 
+git clone https://github.com/andro-sapien/hardware_qcom_audio_msm8996.git -b 11.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/andro-sapien/hardware_qcom_display_msm8996.git -b 11.0-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/andro-sapien/hardware_qcom_media_msm8996.git -b 11.0-caf-msm8996 hardware/qcom-caf/msm8996/media 
 rm -rf hardware/qcom-caf/wlan && git clone https://github.com/andro-sapien/hardware_qcom_wlan.git -b 12 hardware/qcom-caf/wlan
 echo "..."
 echo "..."
