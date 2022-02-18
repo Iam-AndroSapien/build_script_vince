72a6b7cc43e5020e4e84838290b36e54ed3832f7
andro-sapien <alanmadathil007@gmail.com>
2022-02-18 21:16:54 +0530

    switch to aosp clang 14.0
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -19,6 +19,6 @@ echo "..."
 echo "..."
 echo "..."
 echo " clang - 14.0 .1"
-git clone --depth 1 https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b.git prebuilts/clang/host/linux-x86/clang-r437112b
+git clone --depth 1 https://github.com/ArrowOS-Devices/android_prebuilts_clang_host_linux-x86_clang-r437112.git prebuilts/clang/host/linux-x86/clang-r437112
 echo "finishing...."
 echo "All Done."
