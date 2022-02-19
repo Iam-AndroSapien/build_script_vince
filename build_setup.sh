4fad19ae07327aa1d391be41042a6a9d54d84d10
andro-sapien <alanmadathil007@gmail.com>
2022-02-19 08:29:14 +0530

    track hals from gitlab
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -9,9 +9,9 @@ echo "..."
 echo "..."
 echo " Now some miscellaneous things"
 rm -rf hardware/qcom-caf/msm8996
-git clone https://github.com/andro-sapien/hardware_qcom_audio_msm8996.git hardware/qcom-caf/msm8996/audio
-git clone https://github.com/andro-sapien/hardware_qcom_display_msm8996.git hardware/qcom-caf/msm8996/display
-git clone https://github.com/andro-sapien/hardware_qcom_media_msm8996.git hardware/qcom-caf/msm8996/media 
+git clone https://gitlab.com/androsapien/hardware_qcom-caf_msm8996_audio.git hardware/qcom-caf/msm8996/audio
+git clone https://gitlab.com/androsapien/hardware_qcom-caf_msm8996_display.git hardware/qcom-caf/msm8996/display
+git clone https://gitlab.com/androsapien/hardware_qcom-caf_msm8996_media.git hardware/qcom-caf/msm8996/media 
 rm -rf hardware/qcom-caf/wlan && git clone https://github.com/andro-sapien/hardware_qcom_wlan.git -b 12 hardware/qcom-caf/wlan
 git clone https://github.com/andro-sapien/vendor_prebuilts_dirac.git vendor/xiaomi/dirac
 echo "..."
