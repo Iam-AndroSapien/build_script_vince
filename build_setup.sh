321502c6d6fc552ac14bffb2cd6b324f25e5cbc5
andro-sapien <alanmadathil007@gmail.com>
2022-03-11 20:39:41 +0530

    Update branch
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>
    Change-Id: I68498c34161591b7ab3f1f9ae6576f4e417a6ab6

--- build_setup.sh
+++ build_setup.sh
@@ -2,7 +2,7 @@
 cd ../
 echo "starting setting up device side repo...."
 echo "Kernel tree.."
-git clone --depth 1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git -b stable kernel/xiaomi/vince
+git clone --depth 1 https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git -b 12 kernel/xiaomi/vince
 echo "..."
 echo "..."
 echo "..."
